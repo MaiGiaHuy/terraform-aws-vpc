a52c35a21698dafb8598be8bd5d63c30c82f3036
HuyMai <33769653+MaiGiaHuy@users.noreply.github.com>
2023-03-27 10:33:21 +0700

    Delete variable.tf

--- variable.tf
+++
@@ -1,9 +0,0 @@
-variable "instance_type" {
-  type = string
-  description = "Instance type of the EC2"
-
-  validation {
-    condition = contains(["t2.micro"], var.instance_type)
-    error_message = "Value not allow"
-}
-}
